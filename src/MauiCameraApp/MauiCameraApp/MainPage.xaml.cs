f40869aa6606993cd7fdecfc58ee157cd1c84458
niimima <niimi@dcinc.co.jp>
2022-03-22 17:24:30 +0900

    F Androidカメラで写真を撮影できるかを動作確認

--- src/MauiCameraApp/MauiCameraApp/MainPage.xaml.cs
+++ src/MauiCameraApp/MauiCameraApp/MainPage.xaml.cs
@@ -4,17 +4,67 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
 
+    string PhotoPath { get; set; }
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
 		CounterLabel.Text = $"Current count: {count}";
 
 		SemanticScreenReader.Announce(CounterLabel.Text);
+
+        await TakePhotoAsync();
 	}
+
+    /// <summary>
+    /// 写真を撮影する
+    /// </summary>
+    /// <remarks>
+    /// 参考記事：
+    /// https://docs.microsoft.com/ja-jp/xamarin/essentials/media-picker?tabs=android
+    /// </remarks>
+    async Task TakePhotoAsync()
+    {
+        try
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+            await LoadPhotoAsync(photo);
+            Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+        }
+        catch (FeatureNotSupportedException fnsEx)
+        {
+            // Feature is not supported on the device
+        }
+        catch (PermissionException pEx)
+        {
+            // Permissions not granted
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+        }
+    }
+
+    async Task LoadPhotoAsync(FileResult photo)
+    {
+        // canceled
+        if (photo == null)
+        {
+            PhotoPath = null;
+            return;
+        }
+        // save the file into local storage
+        var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+        using (var stream = await photo.OpenReadAsync())
+        using (var newStream = File.OpenWrite(newFile))
+            await stream.CopyToAsync(newStream);
+
+        PhotoPath = newFile;
+    }
 }
 
