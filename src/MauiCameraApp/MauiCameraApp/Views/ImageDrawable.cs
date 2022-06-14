fc8078aa43e441eabde5482015701a9d12a9368a
niimima <niimi@dcinc.co.jp>
2022-06-14 14:42:09 +0900

    F イメージを描画後、タップすると赤い線が引けるように実装

---
+++ src/MauiCameraApp/MauiCameraApp/Views/ImageDrawable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Graphics.Platform;
+
+namespace MauiCameraApp.Views
+{
+    /// <summary>
+    /// 画像を描画くするクラス
+    /// </summary>
+    public class ImageDrawable : IDrawable
+    {
+        /// <summary>
+        /// 画像パス
+        /// </summary>
+        public string ImagePath { get; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="path">画像パス</param>
+        public ImageDrawable(string path)
+        {
+            ImagePath = path;
+        }
+
+        /// <summary>
+        /// キャンバスに描画する
+        /// </summary>
+        /// <param name="canvas"></param>
+        /// <param name="dirtyRect"></param>
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            // 画像読み込み
+            // https://docs.microsoft.com/ja-jp/dotnet/maui/user-interface/graphics/draw#draw-an-image
+            // https://docs.microsoft.com/en-us/dotnet/maui/user-interface/graphics/images
+            Microsoft.Maui.Graphics.IImage image;
+            using (FileStream fs = File.OpenRead(ImagePath))
+            {
+                image = PlatformImage.FromStream(fs);
+            }
+            // デバイスサイズの取得
+            // https://stackoverflow.com/questions/70712367/net-maui-get-screen-y-and-x
+            // var width = (float)DeviceDisplay.MainDisplayInfo.Width;
+            // var height = (float)DeviceDisplay.MainDisplayInfo.Height;
+
+            // イメージサイズの変更
+            // https://docs.microsoft.com/ja-jp/dotnet/maui/user-interface/graphics/images
+            // Microsoft.Maui.Graphics.IImage newImage = image.Resize(width, height);
+
+            canvas.DrawImage(image, 0, 0, dirtyRect.Width, dirtyRect.Height);
+        }
+    }
+}
--- src/MauiCameraApp/MauiCameraApp/Views/PhotoEditorPage.xaml.cs
+++ src/MauiCameraApp/MauiCameraApp/Views/PhotoEditorPage.xaml.cs
@@ -1,3 +1,4 @@
+using MauiCameraApp.ViewModels;
 using MauiCameraApp.Views;
 
 namespace MauiCameraApp;
@@ -15,7 +16,7 @@ public partial class PhotoEditorPage : ContentPage
 
         // タップジェスチャにイベントハンドラを登録する
         // https://stackoverflow.com/questions/71911160/how-to-capture-global-touch-events-in-maui-app
-		var tapGestureRecognizer = new TapGestureRecognizer();
+        var tapGestureRecognizer = new TapGestureRecognizer();
         tapGestureRecognizer.Tapped += TapGestureRecognizer_Tapped;
         m_GraphicsView.GestureRecognizers.Add(tapGestureRecognizer);
 	}
@@ -33,6 +34,23 @@ public partial class PhotoEditorPage : ContentPage
     {
         // 赤い線を引くのみ
         m_GraphicsView.Drawable = new GraphicsDrawable();
+    }
+
+    #endregion
+
+    #region オーバーライド
+
+    /// <summary>
+    /// バインディングコンテキスト変更後処理
+    /// </summary>
+    protected override void OnBindingContextChanged()
+    {
+        // グラフィックビューに画像を描画
+        var vm = BindingContext as PhotoViewModel;
+        if (vm != null && string.IsNullOrEmpty(vm.FilePath) == false)
+        {
+            m_GraphicsView.Drawable = new ImageDrawable(vm.FilePath);
+        }
     }
 
     #endregion
